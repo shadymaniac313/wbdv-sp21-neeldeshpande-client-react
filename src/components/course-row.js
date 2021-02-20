2ba74a47106d29cbf0006009f2409f7fe98bab98
Neel Deshpande <deshpande.nee@husky.neu.edu>
2021-02-20 18:18:24 -0500

    added course row component

---
+++ src/components/course-row.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const CourseRow = () => {
+    return(
+        <tr>
+            <td>
+                CourseName
+            </td>
+            <td>
+                owner
+            </td>
+            <td>
+                lastmodified
+            </td>
+            <td>
+                Action
+            </td>
+        </tr>
+    )
+}
+
+export default CourseRow

10187b24cce040fea98266eb3dde12449b1ce79c
Neel Deshpande <deshpande.nee@husky.neu.edu>
2021-02-20 17:30:37 -0500

    added CRUD services

--- src/courses/course-service.js
+++ src/courses/course-service.js
@@ -1,5 +1,46 @@
 const COURSES_URL = "https://wbdv-generic-server.herokuapp.com/api/001327792/courses";
 
+export const createCourse = (course) =>
+    fetch(COURSES_URL, {
+        method: 'POST',
+        body: JSON.stringify(course),
+        headers: {
+            'content-type': 'application/json'
+        }
+    })
+        .then(response => response.json())
+
 export const findAllCourses = () =>
     fetch(COURSES_URL)
         .then(response => response.json())
+
+export const findCourseById = (courseId) =>
+    fetch(`${COURSES_URL}/${courseId}`)
+        .then(response => response.json())
+
+export const updateCourse = (id, course) => {
+    fetch(`${COURSES_URL}/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify(course),
+        headers: {
+            'content-type': 'application/json'
+        }
+    })
+        .then(response => response.json())
+}
+
+export const deleteCourse = (courseId) =>
+    fetch(`${COURSES_URL}/${courseId}`, {
+        method: 'DELETE'
+    })
+        .then(response => response.json())
+
+const api = {
+    createCourse: createCourse,
+    findAllCourses: findAllCourses,
+    findCourseById: findCourseById,
+    updateCourse: updateCourse,
+    deleteCourse: deleteCourse,
+}
+
+export default api;
